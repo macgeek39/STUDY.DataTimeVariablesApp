f23a3841991f829d10d4e42be037a421cca548d3
Taiwei Ko <152499488+macgeek39@users.noreply.github.com>
2024-01-01 03:43:21 -0600

    learn date only

--- DataTimeVariables/Program.cs
+++ DataTimeVariables/Program.cs
@@ -1,10 +1,16 @@
 ﻿
 
 
-DateTime today = DateTime.Now;
-
+//DateTime today = DateTime.Now;
 //DateTime birthday = DateTime.Parse("6/11/1983");
-
 //DateTime birthday = DateTime.ParseExact("6/11/1983", "d/M/yyyy", CultureInfo.InvariantCulture);
+//Console.WriteLine(today.ToString("MMMM dd, yyyy hh:mm tt zzz"));
 
-Console.WriteLine(today.ToString(format: "MMMM dd, yyyy hh:mm tt zzz"));
+//Date Only
+
+DateTime today = DateTime.Now;
+DateOnly birthday = DateOnly.Parse("5/17/2028");
+Console.WriteLine(birthday.ToString("MMMM dd, yyyy"));
+
+Console.WriteLine($"Today just date: {today.Date}");
+Console.WriteLine($"Birthday full format: {birthday}");
